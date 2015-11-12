379df1a57a1d2b90e7b6af2b45b0b01b3cf88126
Tyler Smart <tyleresmart@gmail.com>
2015-11-12 08:56:24 -0500

    Found the wonderland number

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,31 @@
 (ns wonderland-number.finder)
 
+(defn myhasAllTheSameDigits? [n1 n2]
+  (let [s1 (set (str n1))
+        s2 (set (str n2))]
+    (= s1 s2)))
+
+(defn my-checker [wondernum]
+      (and
+        (= 6 (count (str wondernum)))
+        (myhasAllTheSameDigits? wondernum (* 2 wondernum))
+        (myhasAllTheSameDigits? wondernum (* 3 wondernum))
+        (myhasAllTheSameDigits? wondernum (* 4 wondernum))
+        (myhasAllTheSameDigits? wondernum (* 5 wondernum))
+        (myhasAllTheSameDigits? wondernum (* 6 wondernum))
+      )
+  )
+
+(defn find-first
+         [f coll]
+         (first (filter f coll)))
+
+;;142879
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (find-first
+     #(
+       my-checker %1
+      )
+     (range 100000 200000)
+    )
+  )
